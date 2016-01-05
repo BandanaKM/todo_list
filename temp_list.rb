06f1e0e0d608970ea595d4c4e82249bdb52ca854
Victor Goff <keeperotphones@gmail.com>
2016-01-04 21:30:52 -0500

    Style Guide changes

--- temp_list.rb
+++ temp_list.rb
@@ -1,11 +1,15 @@
-# Remember, there are four high-level responsibilities, each of which have multiple sub-responsibilities:
+# Remember, there are four high-level responsibilities, each of which have
+# multiple sub-responsibilities:
 # 1. Gathering user input and taking the appropriate action (controller)
 # 2. Displaying information to the user (view)
-# 3. Reading and writing from the todo.txt file (persisting models to non-volatile storage, aka "the hard drive")
-# 4. Manipulating the in-memory objects that model a real-life TODO list (model)
+# 3. Reading and writing from the todo.txt file (persisting models to
+# non-volatile storage, aka "the hard drive")
+# 4. Manipulating the in-memory objects that model a real-life TODO list
+# (model)
 
 # Note that (4) is where the essence of your application lives.
-# Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
+# Pretty much every application in the universe has some version of
+# responsibilities (1), (2), and (3).
 
 class List
   attr_reader :all_tasks, :file_name, :num_of_tasks
@@ -15,40 +19,39 @@ class List
   end
 
   def parse
-    File.open(file_name, "r").each do |task|
+    File.open(file_name, 'r').each do |task|
       new_task = Task.new(task.chomp)
-      if task.include?("[X]")
-        new_task.complete = true
-      end
+      new_task.complete = true if task.include?('[X]')
       strings_only(new_task)
       @all_tasks << new_task
     end
   end
-#Method strips checkboxes and leading whitespaces from
-#task descriptions
+
+  # Method strips checkboxes and leading whitespaces from
+  # task descriptions
   def strings_only(task)
     task = task.to_s
-    task.gsub!("[X]", "").lstrip! if task.include?("[X]")
-    task.gsub!("[ ]", "").lstrip! if task.include?("[ ]")
+    task.gsub!('[X]', '').lstrip! if task.include?('[X]')
+    task.gsub!('[ ]', '').lstrip! if task.include?('[ ]')
   end
 
   def display_list
     @all_tasks.map.with_index do |task, index|
-      index+=1
+      index += 1
       "#{index}) #{checkbox?(task)}"
     end.join("\n")
   end
 
-  def checkbox?(task, index = nil)
+  def checkbox?(task, _index = nil)
     if task.complete?
-        "[X] #{task}"
-      else
-        "[ ] #{task}"
-      end
+      "[X] #{task}"
+    else
+      "[ ] #{task}"
+    end
   end
 
   def save_list
-    new_file = File.open(file_name, "w")
+    new_file = File.open(file_name, 'w')
     @all_tasks.each do |task|
       new_file.puts checkbox?(task)
     end
@@ -62,9 +65,9 @@ class List
   def find_task(task_num)
     num_of_tasks = @all_tasks.length
     if (1..num_of_tasks).include?(task_num)
-       return @all_tasks[task_num -1]
+      return @all_tasks[task_num - 1]
     else
-      return "Task not found."
+      return 'Task not found.'
     end
   end
 
@@ -78,7 +81,6 @@ class List
     completed_task.check_off_task
     completed_task.complete = true
   end
-
 end
 
 class Task
@@ -87,48 +89,53 @@ class Task
     @description = description
     @complete = false
   end
-#delete these two methods and assign checkbox status at List#display_list method
+
+  # delete these two methods and assign checkbox status at List#display_list
+  # method
   def check_off_task
-    self.description.gsub!("[ ]", "[X]")
+    description.gsub!('[ ]', '[X]')
   end
 
   def uncheck_task
-    self.description.gsub!("[X]", "[ ]")
+    description.gsub!('[X]', '[ ]')
+  end
+
+  def complete?
+    complete
   end
 
   def to_s
-    self.description
+    description
   end
-
 end
 
 class UserView
   def self.machine_confirmation(command, task)
     case command
-    when "add"
+    when 'add'
       puts "\"#{task}\" has been added to your list!"
-    when "complete"
+    when 'complete'
       puts "\"#{task}\" has been completed!"
-    when "delete"
+    when 'delete'
       puts "\"#{task}\" has been deleted!"
     end
   end
 end
 
 # controller
-test_list = List.new("test_list.txt")
+test_list = List.new('test_list.txt')
 test_list.parse
 command, *rest = ARGV
-rest = rest.join(" ")
+rest = rest.join(' ')
 case command
-when "print"
+when 'print'
   puts test_list.display_list
-when "add"
+when 'add'
   new_task = Task.new(rest)
   test_list.add_task(new_task)
   test_list.save_list
   UserView.machine_confirmation(command, rest)
-when "complete"
+when 'complete'
   rest = rest.to_i
   task = test_list.find_task(rest)
   if test_list.all_tasks.include?(task)
@@ -136,7 +143,7 @@ when "complete"
     test_list.save_list
     UserView.machine_confirmation(command, task)
   end
-when "delete"
+when 'delete'
   rest = rest.to_i
   task = test_list.find_task(rest)
   if test_list.all_tasks.include?(task)
@@ -147,19 +154,18 @@ when "delete"
 end
 
 # view
-#possibly superfluous class
+# possibly superfluous class
 
+# DRIVER TESTS
+test_list = List.new('test_list.txt')
+test_list.parse
+p test_list.all_tasks
+puts test_list.display_list
+p '#########################'
+p test_list.find_task(1)
+test_list.delete_task(11)
+puts test_list.display_list
 
-#DRIVER TESTS
-# test_list = List.new("test_list.txt")
-# test_list.parse
-# p test_list.all_tasks
-# puts test_list.display_list
-# p "#########################"
-# p test_list.find_task(1)
-# test_list.delete_task(11)
-# puts test_list.display_list
-
-
-#I want to be able to open a file
-#I want to be able to complete tasks by typing complete followed by task number
+# I want to be able to open a file
+# I want to be able to complete tasks by typing complete followed by task
+# number
